3427ccac03d0aeb1d4de36dec72f6f014d4e4fd9
Evan <59977587+evan5021@users.noreply.github.com>
2020-11-12 23:49:10 -0500

    Add files via upload
    Currently having a problem with only saving one dictionary entry at a time when writing syntax rules.

---
+++ project/parser.py
@@ -0,0 +1,208 @@
+import sys
+import pickle
+
+class Parser:
+
+	def __init__(self,name):
+		self.lexicon = {}
+		self.syntax = {}
+		self.sentence = ''
+		self.sentence_list = []
+		self.sentence_tagged = []
+		self.current_tree = []
+		self.head = 'S'
+
+#Functions
+
+	def welcome_screen(self):
+		print('The head is currently ' + self.head)
+		value = input('Enter command. For help, enter \'help\':\n')
+		if(value == 'lex'):
+			p.begin_lexical_rules()
+		elif(value == 'help'):
+			p.help()
+#			p.welcome_screen()
+		elif(value == 'pparse'):
+			p.parse_POS()
+		elif(value == 'end'):
+			p.end_screen()
+		elif(value == 'parse'):
+			p.parse()
+#			p.welcome_screen()
+		elif(value == 'savesyn'):
+#			f = open('saved_syn.txt', 'wb')
+#			pickle.dump(self.syntax, f)
+#			f.close
+#			p.welcome_screen()
+			p.save_syn()
+#			p.welcome_screen()
+		elif(value == 'loadsyn'):
+#			f = open('saved_syn.txt', 'rb')
+#			self.syntax = pickle.load(f)
+#			f.close
+#			print('Loaded following syntax:\n')
+#			p.view_syntactical_rules()
+#			print()
+#			p.welcome_screen()
+			p.load_syn()
+#			p.welcome_screen()
+		elif(value == 'savelex'):
+#			f = open('saved_lex.txt', 'wb')
+#			pickle.dump(self.lexicon, f)
+#			f.close
+#			p.welcome_screen()
+			p.save_lex()
+#			p.welcome_screen()
+		elif(value == 'loadlex'):
+#			f = open('saved_lex.txt', 'rb')
+#			self.lexicon = pickle.load(f)
+#			f.close
+#			print('Loaded following lexicon:\n')
+#			p.view_lexical_rules()
+#			print()
+#			p.welcome_screen()
+			p.load_lex()
+#			p.welcome_screen()
+		elif(value == 'deletelex'):
+			p.delete_lex()
+#			p.welcome_screen()
+		elif(value == 'deletesyn'):
+			p.delete_syn()
+#			p.welcome_screen()
+		elif(value == 'load'):
+			p.load_syn()
+			p.load_lex()
+		elif(value == 'save'):
+			p.save_syn()
+			p.save_lex()
+		elif(value == 'head'):
+			p.change_head()
+		elif(value == 'syn'):
+			p.begin_syntactic_rules()
+		elif(value == ' view'):
+			p.view_lexical_rules()
+			p.view_syntactic_rules()
+		else:
+			print('--Unrecognized Command--')
+#			p.welcome_screen()
+		p.welcome_screen()
+			
+	def help(self):
+		print('To begin writing lexical rules, please enter \'lex\'\nTo try and parse a sentence with lexical rules, please enter \'pparse\'\nTo save lexical rules, please enter \'savelex\'\nTo load lexical rules, please enter \'loadlex\'\nTo save syntactic rules, please enter \'savesyn\'\nTo load a syntactic rules, please enter \'loadsyn\'\nTo end the program, please enter\'end\'\nTo begin writing syntactic rules, please enter \'syn\'\nTo change the head, please enter \'head\'\nTo load all, please enter \'load\'\nTo save all, please enter \'save\'\nTo delete current lexical entries, please enter \'deletelex\'\nTo delete current syntactic entries, please enter \'deletesyn\'\nTo parse, please enter \'parse\'\nTo view all rules, please enter \'view\'\n')
+	def load_syn(self):
+		f = open('saved_syn.txt', 'rb')
+		self.syntax = pickle.load(f)
+		f.close
+		print('Loaded following syntax:\n')
+		p.view_syntactic_rules()
+		print()
+		
+	def load_lex(self):
+		f = open('saved_lex.txt', 'rb')
+		self.lexicon = pickle.load(f)
+		f.close
+		print('Loaded following lexicon:\n')
+		p.view_lexical_rules()
+		print()
+		
+	def save_syn(self):
+		f = open('saved_syn.txt', 'wb')
+		pickle.dump(self.syntax, f)
+		f.close	
+		
+	def save_lex(self):
+		f = open('saved_lex.txt', 'wb')
+		pickle.dump(self.lexicon, f)
+		f.close
+	
+	def delete_lex(self):
+		self.lexicon = {}
+		
+	def delete_syn(self):
+		self.syntax ={}
+		
+	def end_screen(self):
+		exit()
+	def begin_lexical_rules(self):
+		value = input('If you would like to go back enter \'leave\' if you would like to view all lexical rules, enter \'view\', otherwise press enter\n')
+		if value == 'leave':
+#			p.welcome_screen()
+			pass
+		elif value == 'view':
+			p.view_lexical_rules()
+			p.begin_lexical_rules()
+		else:
+			value1 = input('Please enter part of speech:\n')
+			value2 = input('Please enter word:\n')
+			self.lexicon[value2] = value1
+			p.begin_lexical_rules()
+
+	def view_lexical_rules(self):
+		for key, value in self.lexicon.items():
+			print(key + " has part of speech " + value)
+
+	def view_syntactic_rules(self):
+		print(len(self.syntax))
+		for key, value in self.syntax.items():
+			print(key + ' has daughters ', end='')
+			for item in value:
+				print(item + ' ', end='')
+			print()
+
+	def parse_POS(self):
+		value = input('Please enter the sentence you wish to be parsed:\n')
+		str = value.split(' ')
+		for item in str:
+			POS = self.lexicon[item]
+			print(POS + '(' + item + ') ', end='')
+		print('\nFinished')
+#		p.welcome_screen()
+
+	def parse(self):
+		value = input('Please enter the sentence you wish to be parsed:\n')
+		self.sentence = value
+		self.sentence_list = value.split(' ')
+		print(self.sentence_list)
+		
+		for item in self.sentence_list:
+			print(item)
+			self.sentence_tagged[p.find_POS(item)] = item
+			print(self.sentence_tagged)
+
+	def begin_syntactic_rules(self):
+		value = input('If you would like to go back enter \'leave\' if you would like to view all syntactic rules, enter \'view\', otherwise press enter\n')
+		if value == 'leave':
+#			p.welcome_screen()
+			pass
+		elif value == 'view':
+			p.view_syntactic_rules()
+			p.begin_syntactic_rules()
+		else:
+			value1 = input('Please enter mother:\n')
+			value2 = input('Please enter daughters:\n')
+			self.syntax[value1] = value2.split(' ')
+			print(self.syntax)
+			p.begin_syntactic_rules()
+			
+	def find_POS(self, str):
+		return self.lexicon[str]
+
+	def change_head(self):
+		value = input('Please enter new head:\n')
+		self.head = value
+#		p.welcome_screen()
+
+
+
+
+
+
+
+
+
+#Beginning input
+
+p = Parser('Evan')
+print('Welcome to Evan\'s Parser!')
+p.welcome_screen()
+p.end_screen()
